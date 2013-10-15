3d61c85015e338caae68a120f4437a7261922cf3
Felix Tioh <felixsagitta@gmail.com>
2013-10-15 15:39:46 +0800

    update script

--- gitpull.php
+++ gitpull.php
@@ -2,6 +2,6 @@
 
 echo getcwd() . "\n";
 echo '<p>beginning git pull...</p>';
-echo shell_exec('/usr/bin/git pull 2>&1');
+echo shell_exec('/usr/bin/git pull origin master 2>&1');
 echo '<p>done</p>';
 ?>
