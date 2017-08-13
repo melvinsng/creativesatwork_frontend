bb934f96ecb27a02dda093efcf9c01ef98201af8
felix remote <felixsagitta@gmail.com>
2017-08-13 17:07:57 +0000

    transformation

--- gitpull.php
+++ gitpull.php
@@ -2,6 +2,6 @@
 
 echo getcwd() . "\n";
 echo '<p>beginning git pull -u origin master...</p>';
-echo shell_exec('/usr/bin/git pull -u origin master 2>&1');
+//echo shell_exec('/usr/bin/git pull -u origin master 2>&1');
 echo '<p>done</p>';
 ?>
